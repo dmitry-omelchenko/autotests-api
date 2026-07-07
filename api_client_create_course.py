59b70d389f5c5f33c00a32f7f64adbbd5c13833c
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2026-07-07 22:03:21 +0200

    7.2 Применяем Pydantic в API клиентах. Практикуемся в применении Pydantic в API клиентах

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,4 +1,5 @@
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
+from clients.courses.courses_client import get_courses_client
+from clients.courses.courses_schema import CreateCourseRequestSchema
 from clients.files.files_client import get_files_client
 from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
@@ -37,7 +38,7 @@ create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
-create_course_request = CreateCourseRequestDict(
+create_course_request = CreateCourseRequestSchema(
     title="Python",
     maxScore=100,
     minScore=10,
