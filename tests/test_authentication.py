b2fb817e43dea5730e91cb2c3278b2e12eb38f25
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2026-07-21 23:10:11 +0200

    8.2 Написание API автотестов используя Pytest. Практика написания API автотестов

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from clients.authentication.authentication_client import get_authentication_client
-from clients.authentication.authentication_schema import LoginResponseSchema
+from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 from tools.assertions.authentication import assert_login_response
@@ -9,15 +9,20 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
 
-def test_login():
+def test_login() -> None:
     public_users_client = get_public_users_client()
     authentication_client = get_authentication_client()
 
 
-    request = CreateUserRequestSchema()
-    response = public_users_client.create_user(request)
+    create_user_request = CreateUserRequestSchema()
+    public_users_client.create_user(create_user_request)
 
-    login_response = authentication_client.login_api(request)
+    login_request = LoginRequestSchema(
+        email=create_user_request.email,
+        password=create_user_request.password
+    )
+
+    login_response = authentication_client.login_api(login_request)
     login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
 
     assert_status_code(login_response.status_code, HTTPStatus.OK)
